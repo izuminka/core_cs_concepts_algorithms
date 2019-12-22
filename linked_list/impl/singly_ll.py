bb1293ae11b1443cb4beb2b46017e2d3e44fff87
izuminka <dolin@ucsb.edu>
2019-12-22 15:59:06 -0800

    clss singll new meth. work: back, pop_back, at. made push_back simpler

--- linked_list/impl/singly_ll.py
+++ linked_list/impl/singly_ll.py
@@ -12,6 +12,53 @@ class SinglyLinkedList:
     """
     def __init__(self):
         self.head = None
+        self.size = 0
+
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.val)
+            current = current.next
+
+    def at(self,index):
+        """get Node at the index strating from 0. If beyond len list return None
+
+        Args:
+            index (int): index of the desired Node.
+
+        Returns:
+            Node if index exists, None otherwise.
+
+        """
+        i = 0
+        current = self.head
+        while(i<index and current):
+            current = current.next
+            i+=1
+        return current
+
+    def back(self):
+        """Get last node of the list
+
+        Returns:
+            Node: last node
+
+        """
+        current = self.head
+        while current.next:
+            current = current.next
+        return current
+
+    def pop_back(self):
+        """Remove the last node of the list
+        """
+        if self.size>1:
+            #find a node one less than last one and set next to not
+            self.at(self.size-2).next = None
+        else:
+            self.head = None
+        self.size=-1
 
     def push_back(self,val):
         """Push val at the end of the list
@@ -26,19 +73,16 @@ class SinglyLinkedList:
         if not self.head:
             self.head = Node(val)
         else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = Node(val)
+            self.back().next = Node(val)
+        self.size+=1
 
-    def print_list(self):
-        current = self.head
-        while current:
-            print(current.val)
-            current = current.next
-
-ll_test = SinglyLinkedList()
-ll_test.push_back(1)
-ll_test.push_back(2)
-ll_test.push_back(3)
-ll_test.print_list()
+# ll_test = SinglyLinkedList()
+# ll_test.push_back(0)
+# ll_test.push_back(1)
+# ll_test.push_back(2)
+# ll_test.print_list()
+#
+# ll_test.size
+# ll_test.pop_back()
+# ll_test.back().val
+# ll_test.at(5)
