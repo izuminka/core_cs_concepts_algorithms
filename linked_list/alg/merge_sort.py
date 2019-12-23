7ce4cbb18250d9ad50347f4d6a0c08fdd5b77102
izuminka <dolin@ucsb.edu>
2019-12-22 21:30:33 -0800

    split half function tested, works

--- linked_list/alg/merge_sort.py
+++ linked_list/alg/merge_sort.py
@@ -8,19 +8,37 @@ def print_ll(node):
         print(node.val)
         node = node.next
 
-def test_ll():
-    ls = list(range(9,-1,-1))
+def test_ll(s):
+    ls = list(range(s,-1,-1))
     head = curr = Node(ls[0])
     for v in ls[1:]:
         curr.next = Node(v)
         curr = curr.next
     return head
 
+def split_half(head):
+    if not head or not head.next:
+        return head
+    slow = head.next
+    fast = head.next
+    while fast.next and fast.next.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
 
+# def merge_sort(head):
+    # if not head or not head.next:
+    #     return head
+    # middle_node = split_half(head)#len(ls)//2
+    # left = merge_sort(head)
+    # right = merge_sort(middle_node)
+    # return merge(left, right)
 
-t_ll = test_ll()
+t_ll = test_ll(1)
 print_ll(t_ll)
-#
+split_half(t_ll).val
+
+
 # t = Node(0)
 # t.next = Node(1)
 # t.next.next = Node(3)
